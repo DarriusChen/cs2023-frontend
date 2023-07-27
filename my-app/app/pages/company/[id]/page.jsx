1af7ae6a807a952949f919cb252163c234e8e34d
DarriusChen <darriuschen0926@gmail.com>
2023-07-27 16:36:03 +0800

    feat: add images display area

--- my-app/app/pages/company/[id]/page.jsx
+++ my-app/app/pages/company/[id]/page.jsx
@@ -2,10 +2,44 @@
 import Cstyle from '@/app/styles/company.module.css';
 import Cdata from '@/app/company.json';
 import Link from 'next/link';
+import Image from 'next/image';
+import path from 'path';
+import fs from 'fs';
 
-export default function Importdata({ params: { id } }) {
+export default function ShowCompany({ params: { id } }) {
   //get the company info
-  const info = Cdata.filter((ele) => ele.name == id)[0]; 
+  const info = Cdata.filter((ele) => ele.name == id)[0];
+  // 照片呈現
+  //get images using api
+  async function getImages(id) {
+    const res = await fetch(`http://192.168.70.134:8000/getimages/${id}`);
+    // // The return value is *not* serialized
+    // if (!res.ok) {
+    //   // This will activate the closest `error.js` Error Boundary
+    //   throw new Error('Failed to fetch data');
+    // }
+    // const result = await res.json().then((data) => data);
+    // return result;
+  }
+  const imageurls = getImages(id);
+  // const Gallery = ({ imageurls }) => {
+  //   return (
+  //     <div className={Cstyle.eachDm}>
+  //       {imageurls.map((url,i) => (
+  //         <Image
+  //           className={card}
+  //           width={0}
+  //           height={0}
+  //           style={{width:"3vh",height:"5vh"}}
+  //           alt={`dm+${i}`}
+  //           src={url}
+  //           key={i}
+  //         />
+  //       ))}
+  //     </div>
+  //   )
+  // }
+
   // filter the data to make it remain only the data whose company's name = id
   return (
     <div className={Cstyle.main}>
@@ -17,11 +51,20 @@ export default function Importdata({ params: { id } }) {
       </div>
       <div className={Cstyle.products}>
         Products: &emsp;
-        {info.products.map((tag)=>(
-          <div className={Cstyle.tag}>
-            {tag}
-          </div>
+        {info.products.map((tag) => (
+          <div className={Cstyle.tag}>{tag}</div>
         ))}
+      </div>
+      <div className={Cstyle.dmArea}>
+        <Link className={Cstyle.backArrow} href={''}>
+          <Image src={'/arrow_back.svg'} width={0} height={0} style={{ width: '5vh', height: '7vh' }}></Image>
+        </Link>
+        <div className={Cstyle.dms}>
+          {/* <Gallery images={imageurls} /> */}
+        </div>
+        <Link className={Cstyle.forwardArrow} href={''}>
+          <Image src={'/arrow_forward.svg'} width={0} height={0} style={{ width: '5vh', height: '7vh' }}></Image>
+        </Link>
       </div>
     </div>
   );
