fafe6faed5443461da6061feb10af2ebaa103b77
DarriusChen <darriuschen0926@gmail.com>
2023-11-07 18:11:13 +0800

    feat: change font style and spacing

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,24 +1,24 @@
-import './styles/globals.css'
-import { Gothic_A1 } from 'next/font/google'
-import Nav from './nav'
-import Foo from './footer'
+import './styles/globals.css';
+import { Comfortaa, Outfit, Inconsolata } from 'next/font/google';
+import Nav from './nav';
+import Foo from './footer';
 
-
-const font = Gothic_A1({ subsets: ['latin'], weight:'900' })
+const font = Inconsolata({ subsets: ['latin'], weight:"900", display: 'swap'});
+const outfit = Outfit({ subsets: ['latin'], weight:"600", display: 'swap' })
 
 export const metadata = {
-  title: "cybersecurity products analysis",
-  description: "comparison of different cybersecurity products",
-}
+  title: 'cybersecurity products analysis',
+  description: 'comparison of different cybersecurity products'
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className} suppressHydrationWarning={true}>
+      <body className={outfit.className} suppressHydrationWarning={true}>
         <Nav></Nav>
         {children}
         <Foo></Foo>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
